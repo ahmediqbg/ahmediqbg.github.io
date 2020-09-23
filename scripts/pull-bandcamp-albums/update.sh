945d798d8c47c63e49af7e24113427fbf2833486
Henry Post <henryfbp@gmail.com>
2020-09-23 13:20:45 -0500

    enforce clean working tree to pull+commit bc albums

--- scripts/pull-bandcamp-albums/update.sh
+++ scripts/pull-bandcamp-albums/update.sh
@@ -2,8 +2,17 @@
 
 # Run to update all Bandcamp data yml files.
 
-pipenv run python pull-bandcamp-purchased-albums-to-data.py
-pipenv run python pull-bandcamp-wishlisted-albums-to-data.py
+if [ -z "$(git status --porcelain)" ]; then
+  pipenv run python pull-bandcamp-purchased-albums-to-data.py
+  pipenv run python pull-bandcamp-wishlisted-albums-to-data.py
 
-git add -A
-git commit -m '[AUTO] update bandcamp albums'
+  git add -A
+  git commit -m '[AUTO] update bandcamp albums'
+else
+  git status
+
+  printf '\nERROR: Working tree must be clean. Please commit or stash changes.\n\n'
+
+  exit 1
+fi
+
